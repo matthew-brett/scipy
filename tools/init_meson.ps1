67a21d2ff37db44266d01a2569f9c4581d7f4b35
Matthew Brett <matthew.brett@gmail.com>
2021-11-22 22:35:11 +0000

    Switch to vanilla gcc chain

--- tools/init_meson.ps1
+++ tools/init_meson.ps1
@@ -1,8 +1,8 @@
 # Init scipy build.
-$ucrt_tools = "C:\rtools40"
+# $ucrt_tools = "C:\rtools40"
 # Or:
-# $ucrt_tools = "C:\msys64-ucrt"
+$ucrt_tools = "C:\msys64"
 # Compilers, and make tools.
-$env:PATH = "$ucrt_tools\ucrt64\bin;$ucrt_tools\usr\bin;$env:PATH"
+$env:PATH = "$env:PATH;$ucrt_tools\ucrt64\bin;$ucrt_tools\usr\bin"
 # In order to find OpenBLAS
 $env:PKG_CONFIG_PATH = "c:\opt\openblas\if_32\64\lib\pkgconfig;"
